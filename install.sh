68ec1a58682dbe8e5763f82a661a5a1f06d1350f
Tyler Curtis <tcurtis@vmware.com>
2016-02-18 11:02:46 -0800

    Added commit_template and corrected global git ignore

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 echo "=== Creating symbolic links..."
-ln -s $PWD/profile          $HOME/.profile
-ln -s $PWD/bash_aliases     $HOME/.bash_aliases
-ln -s $PWD/ideavimrc        $HOME/.ideavimrc
-ln -s $PWD/gitconfig        $HOME/.gitconfig
-ln -s $PWD/gitconfig_global $HOME/.gitconfig_global
-ln -s $PWD/vimrc            $HOME/.vimrc
+ln -s $PWD/profile             $HOME/.profile
+ln -s $PWD/bash_aliases        $HOME/.bash_aliases
+ln -s $PWD/ideavimrc           $HOME/.ideavimrc
+ln -s $PWD/gitconfig           $HOME/.gitconfig
+ln -s $PWD/gitignore_global    $HOME/.gitignore_global
+ln -s $PWD/commit_template.txt $HOME/.commit_template.txt
+ln -s $PWD/vimrc               $HOME/.vimrc
 
 echo "=== Sourcing my new .profile"
 source $HOME/.profile
