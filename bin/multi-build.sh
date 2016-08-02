c497e7f70800f33e4e739188c0979194cc376ef6
Tyler Curtis <tcurtis@vmware.com>
2016-08-02 13:06:09 -0700

    multi-build.sh now supports number of iterations

--- bin/multi-build.sh
+++ bin/multi-build.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
+LOG_DIR=~/tmp/multi-build-logs
+DEFAULT_REPEAT=20
+REPEAT=$1
+if [ -z "$REPEAT" ]
+then
+    REPEAT=$DEFAULT_REPEAT
+fi
+echo Running $REPEAT builds, outputing results to: $LOG_DIR
+
 COUNTER=0
-while [  $COUNTER -lt 30 ]; do
-    mvn clean install > ~/tmp/appMigrationLogs/mvn-$COUNTER.log 2>&1
+mkdir -p $LOG_DIR
+while [  $COUNTER -lt $REPEAT ]; do
+    mvn clean install -P backend > $LOG_DIR/mvn-$COUNTER.log 2>&1
+    #mvn -U clean install > $LOG_DIR/mvn-$COUNTER.log 2>&1
     if [ $? -eq 0 ]; then
-        echo Success
-        touch ~/tmp/appMigrationLogs/mvn-$COUNTER.success
+        echo Run $COUNTER: Success
+        touch $LOG_DIR/mvn-$COUNTER.success
     else
-        echo FAILED!
-        touch ~/tmp/appMigrationLogs/mvn-$COUNTER.failure
+        echo Run $COUNTER: FAILURE
+        touch $LOG_DIR/mvn-$COUNTER.failure
     fi
 
     let COUNTER=COUNTER+1 
