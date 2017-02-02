13ce767fa356a1b8202956196a100565284df4ad
Tyler Curtis <tcurtis@vmware.com>
2017-02-02 13:48:56 -0800

    Create helpful docker tag for upgrade testing

--- bin/rebuild-symphony.sh
+++ bin/rebuild-symphony.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -vx
 
 cd ~/workspace/symphony
-#mvn clean install -P backend -DskipTests
-mvn clean install -DskipTests
+mvn clean install -P backend -DskipTests
+#mvn clean install -DskipTests
+#mvn clean install
 if [ $? -ne 0 ] ; then exit 1; fi
 
 make docker-local
+
+# Helpful for testing upgrade
+docker tag symphony-docker-local.jfrog.io/symphony/symphony:custom symphony-docker-local.jfrog.io/symphony/symphony:custom2
